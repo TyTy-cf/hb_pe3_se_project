4ce0e8496197993c12347f09b28263d7c69a4261
Kevin Tourret <tarpion@toto.fr>
2022-02-22 17:00:44 +0100

    correction exo pagination complète

--- src/app/pokedex/pokedex.component.ts
+++ src/app/pokedex/pokedex.component.ts
@@ -8,14 +8,52 @@ import { Component, OnInit } from '@angular/core';
 export class PokedexComponent implements OnInit {
 
   arrayPokemonIndex: string[] = [];
+  displayedByPage: number = 9; // nb pokemons by page
+  startingIndex: number = 0;
+  endingIndex: number = this.displayedByPage;
+  currentPage: number = 1;
 
   constructor() {
-    for(let i = 1; i <= 9; i++) {
+    for(let i = 1; i <= 151; i++) {
       this.arrayPokemonIndex.push('https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/'+i+'.png');
     }
+    console.log(this.arrayPokemonIndex);
   }
 
   ngOnInit(): void {
   }
 
+  changePage(nb: number = 1) {
+    this.currentPage += nb;
+    // On essaie d'aller à la page suivante : on change les valeurs de startIndex et endIndex
+    // En leur ajoutant la valeur du nombre de données à afficher par page
+    // Ou en leur enlevant la valeur du nombre de données à afficher par page
+    if (nb > 0) {
+      this.startingIndex += this.displayedByPage;
+      this.endingIndex += this.displayedByPage;
+    } else {
+      this.startingIndex -= this.displayedByPage;
+      this.endingIndex -= this.displayedByPage;
+    }
+  }
+
+  getTotalPage(): number {
+    return Math.ceil(this.arrayPokemonIndex.length/this.displayedByPage);
+  }
+
+  changeDisplayed(newDisplayedByPage: number): void {
+    // On récupère l'ancienne valeur de page max
+    const oldCurrentMaxPage: number = this.getTotalPage();
+    // On affecte la nouvelle valeur d'affichage par page
+    this.displayedByPage = newDisplayedByPage;
+    // On récupère la nouvelle page max
+    const newMaxPage: number = this.getTotalPage();
+    // Si l'ancienne est plus grande que la nouvelle, alors la page actuelle devient la nouvelle
+    if (oldCurrentMaxPage > newMaxPage) {
+      this.currentPage = newMaxPage;
+    }
+    // On met à jour nos index de slice
+    this.endingIndex = newDisplayedByPage * this.currentPage;
+    this.startingIndex = this.endingIndex - newDisplayedByPage;
+  }
 }
