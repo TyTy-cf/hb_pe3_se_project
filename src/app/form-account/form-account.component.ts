46a6888df9525884613f0079f1f93fc0fe9eaccc
Kevin Tourret <tarpion@toto.fr>
2022-03-08 14:38:37 +0100

    finalisation du psot dans le submit du form + redirection en .ts

--- src/app/form-account/form-account.component.ts
+++ src/app/form-account/form-account.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { faEye } from '@fortawesome/free-solid-svg-icons';
 import {Account} from "../../models/api-steam/account";
 import {ApiRequestService} from "../../service/api-request.service";
+import {Router} from "@angular/router";
 
 @Component({
   selector: 'app-form-account',
@@ -16,15 +17,29 @@ export class FormAccountComponent implements OnInit {
   maxNicknameCharacters: number = 3;
   tmpCheckPassword: string = '';
   typeInput: string = 'password';
+  alertAccountOk: boolean = false;
 
-  constructor(private apiRequestService: ApiRequestService) { }
+  // import {Router} from "@angular/router";
+  constructor(
+    private apiRequestService: ApiRequestService,
+    private router: Router
+  ) { }
 
   ngOnInit(): void {
   }
 
-  submit(): void {
-    this.apiRequestService.createAccount(this.account).subscribe();
-
+  // async : mot clé indiquant que la fonction effectue un traitement "asynchrone"
+  // Promise<void> : une fonction ayant un traitement asynchrone, renvoie toujours une Promise
+  // Ici la fonction n'a pas de return, donc une Promise vide (Promise<void>)
+  async submit(): Promise<void> {
+    // On doit préciser async aussi devant notre fonction anonyme
+    this.apiRequestService.createAccount(this.account).subscribe(async (data) => {
+      if (data) {
+        this.alertAccountOk = true;
+        // on place le "await" devant le traitement asynchrone
+        await this.router.navigate(['/account']);
+      }
+    });
   }
 
 }
